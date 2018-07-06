59e4f17ea00173760acb9d77329bfc3e207fd3f8
bucklb <bucklb@hotmail.com>
2018-07-06 21:09:18 +0100

    try and get to github

---
+++ src/main/java/com/mkyong/SpringBootConsoleApplication.java
@@ -0,0 +1,49 @@
+package com.mkyong;
+
+import com.mkyong.service.HelloMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+import static java.lang.System.exit;
+import static java.lang.System.setOut;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+
+    @Autowired
+    private HelloMessageService helloService;
+
+    public static void main(String[] args) throws Exception {
+
+        //disabled banner, don't want to see the spring logo
+        SpringApplication app = new SpringApplication(SpringBootConsoleApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
+        //SpringApplication.run(SpringBootConsoleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        // Put some space bewteen us and the "reflection" moaning ...
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+
+        if (args.length > 0 ) {
+            System.out.println(helloService.getMessage(args[0].toString()));
+        }else{
+            System.out.println(helloService.getMessage());
+        }
+
+        exit(0);
+    }
+}
